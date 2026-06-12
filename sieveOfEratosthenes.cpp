c29a3e2768c4941117738d61abe118bd4b242515
Dipanjan Sadhukhan <dipanjansadhukhan123@gmail.com>
2026-06-12 17:49:28 +0530

    New program

---
+++ sieveOfEratosthenes.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+class Solution
+{
+public:
+    int compress(vector<char> &chars)
+    {
+        int n = chars.size();
+        int idx = 0;
+        for (int i = 0; i < n; i++)
+        {
+            char ch = chars[i];
+            int count = 0;
+            while (i < n && chars[i] == ch)
+            {
+                count++;
+                i++;
+            }
+
+            if (count == 1)
+                chars[idx++] = ch;
+            else
+            {
+                chars[idx++] = ch;
+                for (char dig : to_string(count))
+                    chars[idx++] = dig;
+            }
+            i--;
+        }
+
+        chars.resize(idx);
+        return idx;
+    }
+};
+int main()
+{
+    cout << "Hello, World!" << endl;
+    return 0;
+}
